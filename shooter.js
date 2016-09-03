dd8a59ed6b0bf577c69d7305581d648d4ee4cb7c
Joshua Bumgarner <bobulousness@gmail.com>
2016-09-03 11:42:34 -0400

    Update shooter.js

--- shooter.js
+++ shooter.js
@@ -6,5 +6,5 @@
 
 -----------------------------------------------------------------------------------------------------------------------
 var game = new Phaser.Game(320,568);
-game.state.add('play', Dodge.Play);
+game.state.add('play', shooter.Play);
 game.state.start('play');
