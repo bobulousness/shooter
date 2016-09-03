e1ef97aec98948373ea886ac21d5bf00473f9592
Joshua Bumgarner <bobulousness@gmail.com>
2016-09-03 11:54:05 -0400

    Update shooter.js

--- shooter.js
+++ shooter.js
@@ -15,7 +15,7 @@ class PlayState {
 
 }
 
------------------------------------------------------------------------------------------------------------------------
+
 var game = new Phaser.Game(320,568);
 game.state.add('play', shooter.Play);
 game.state.start('play');
